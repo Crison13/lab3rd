d813799165817761c377f4fb048c7cc0e986899d
admin <admin@Bcalab4-m18>
2022-03-16 09:23:14 +0530

    commit1

---
+++ GitHub/src/Student.java
@@ -0,0 +1,13 @@
+
+public class Student {
+
+	Student()
+	{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Stdent Detail:");
+	}
+
+}
